e72e4ed55ac4cf885f20c90b55c419706a10c185
ARunni <aruncmunni@gmail.com>
2025-11-06 19:19:05 +0530

    Remove MongoDB-related code from warehouse model and clean up comments

--- models/warehouse.go
+++ models/warehouse.go
@@ -6,22 +6,6 @@ import (
 	"gorm.io/gorm"
 )
 
-// type Warehouse struct {
-// 	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-// 	Name          string             `bson:"name" json:"name"`
-// 	Location      string             `bson:"location" json:"location"`
-// 	TotalArea     float64            `bson:"total_area" json:"total_area"` // in square feet
-// 	AvailableArea float64            `bson:"available_area" json:"available_area"`
-// 	RentConfig    RentRate           `bson:"rent_config" json:"rent_config"`
-// 	CreatedAt     time.Time          `bson:"created_at" json:"created_at"`
-// 	UpdatedAt     time.Time          `bson:"updated_at" json:"updated_at"`
-// }
-
-// type RentRate struct {
-// 	RatePerSqft  float64 `bson:"rate_per_sqft" json:"rate_per_sqft"`
-// 	Currency     string `bson:"currency" json:"currency"`
-// 	BillingCycle string `bson:"billing_cycle" json:"billing_cycle"`
-// }
 
 type Warehouse struct {
 	ID            uint           `gorm:"primaryKey;autoIncrement" json:"id"`
